14f5cf23cf9716616fd9bb13f0e0333a8cc1497e
George Andronache <georgeandronache.cpp@gmail.com>
2023-02-07 22:18:55 +0200

    tab navigator adjust #1 -- prepare vitals charts component

--- client/src/components/CustomDrawer.js
+++ client/src/components/CustomDrawer.js
@@ -57,12 +57,13 @@ const CustomDrawer = props => {
       <View style={{padding: 20, borderTopWidth: 1, borderTopColor: '#ccc'}}>
         <TouchableOpacity onPress={() => {}} style={{paddingVertical: 15}}>
           <View style={{flexDirection: 'row', alignItems: 'center'}}>
-            <Ionicons name="share-social-outline" size={22} />
+            <Ionicons name="share-social-outline" size={22} color='#333' />
             <Text
               style={{
                 fontSize: 15,
                 fontFamily: 'Roboto-Medium',
                 marginLeft: 5,
+                color: '#333'
               }}>
               Tell a Friend
             </Text>
@@ -70,12 +71,13 @@ const CustomDrawer = props => {
         </TouchableOpacity>
         <TouchableOpacity onPress={() => {logout()}} style={{paddingVertical: 15}}>
           <View style={{flexDirection: 'row', alignItems: 'center'}}>
-            <Ionicons name="exit-outline" size={22} />
+            <Ionicons name="exit-outline" size={22} color='#333' />
             <Text
               style={{
                 fontSize: 15,
                 fontFamily: 'Roboto-Medium',
                 marginLeft: 5,
+                color: '#333'
               }}>
               Sign Out
             </Text>
