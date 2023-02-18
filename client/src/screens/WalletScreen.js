75bdf2ad195ad38f2de18feaf288491c660d0e7d
George Andronache <georgeandronache.cpp@gmail.com>
2023-02-18 21:36:31 +0200

    wallet screen UI WiP #1

--- client/src/screens/WalletScreen.js
+++ client/src/screens/WalletScreen.js
@@ -16,8 +16,7 @@ import { AuthContext } from '../context/AuthContext';
 import "@ethersproject/shims"
 import { ethers } from "ethers";
 import { contract_address, contractABI } from '../web3/constants';
-
-
+import MetamaskSVG from '../assets/images/misc/metamask-fox.svg';
 
 
 const WalletScreen = ({navigation}) => {
@@ -25,8 +24,6 @@ const WalletScreen = ({navigation}) => {
   const {userInfo} = useContext(AuthContext);
   const connector = useWalletConnect(); // Wallet connect hook
   const [balanceDAC, setBalanceDAC] = useState(0.0);
-
-
   const fetchBalanceDAC = async (addressFrom) => {
     const provider = new ethers.providers.AlchemyProvider('goerli', 'vlYolnH8xOcJ_nq6M0Edtj_KmkEGZTnw')
     const contractDAC = new ethers.Contract(contract_address, contractABI, provider);
@@ -48,61 +45,143 @@ const WalletScreen = ({navigation}) => {
   }, [connector]); // Only re-run the effect if count changes
 
   return (
-    <SafeAreaView style={{flex: 1, backgroundColor: '#1f2026'}}>
-      <ScrollView style={{padding: 20}}>
-        <View
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            alignSelf: 'flex-end',
-            marginBottom: 20,
-          }}>
+      <SafeAreaView style={styles.container}>
+      <ScrollView contentContainerStyle={styles.scrollContainer}>
+        <View style={styles.header}>
           <TouchableOpacity onPress={() => navigation.openDrawer()}>
             <ImageBackground
-              source={{uri:userInfo.data.photo}}
-              style={{width: 35, height: 35}}
-              imageStyle={{borderRadius: 25}}
+              source={{ uri: userInfo.data.photo }}
+              style={styles.userAvatar}
+              imageStyle={{ borderRadius: 25 }}
             />
           </TouchableOpacity>
         </View>
         {!connector.connected ? 
-          <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
-            <TouchableOpacity onPress={authenticateUser}>
-              <Text>Connect to Wallet</Text>
+          <View style={styles.connectWallet}>
+            <TouchableOpacity onPress={authenticateUser} style={styles.connectButton}>
+              <Text style={styles.connectButtonText}>Connect to Wallet</Text>
             </TouchableOpacity>
           </View>
         : 
-          <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
-            <Text>Connected account: {connector.session.accounts[0].slice(0,10)}</Text>
-            <Text>Wallet provider: {connector.session.peerMeta.name}</Text>
-            <Text>DAC balance: {balanceDAC}</Text>
-            <Image
-              style={styles.logo}
-              source={{
-                uri: connector.session.peerMeta.icons[0],
-              }}
+          <View style={styles.connectedWallet}>
+            <View style={styles.accountInfo}>
+              <Text style={styles.accountLabel}>Connected account:</Text>
+              <Text style={styles.accountValue}>{connector.session.accounts[0].slice(0,10)}</Text>
+            </View>
+            <View style={styles.walletInfo}>
+              <Text style={styles.walletLabel}>Wallet provider:</Text>
+              <Text style={styles.walletValue}>{connector.session.peerMeta.name}</Text>
+            </View>
+            <View style={styles.balanceInfo}>
+              <Text style={styles.balanceLabel}>DAC balance:</Text>
+              <Text style={styles.balanceValue}>{balanceDAC}</Text>
+            </View>
+            <MetamaskSVG
+            height={300}
+            width={300}
             />
-            <TouchableOpacity onPress={() => {connector.killSession();}}>
-              <Text>Kill session</Text>
+            <TouchableOpacity onPress={() => {connector.killSession();}} style={styles.disconnectButton}>
+              <Text style={styles.disconnectButtonText}>Disconnect Wallet</Text>
             </TouchableOpacity>
           </View>
-      }
+        }
       </ScrollView>
-  </SafeAreaView>
+    </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    paddingTop: 50,
+    flex: 1,
+    backgroundColor: '#1f2026',
   },
-  tinyLogo: {
-    width: 50,
-    height: 50,
+  scrollContainer: {
+    padding: 20,
   },
-  logo: {
-    width: 66,
-    height: 58,
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignSelf: 'flex-end',
+    marginBottom: 20,
+  },
+  userAvatar: {
+    width: 35,
+    height: 35,
+    borderRadius: 25,
+  },
+  connectWallet: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  connectButton: {
+    backgroundColor: '#007AFF',
+    borderRadius: 8,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+  },
+  connectButtonText: {
+    color: '#FFFFFF',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  connectedWallet: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  accountInfo: {
+    flexDirection: 'row',
+    marginBottom: 10,
+  },
+  accountLabel: {
+    color: '#FFFFFF',
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginRight: 10,
+  },
+  accountValue: {
+    color: '#FFFFFF',
+    fontSize: 18,
+  },
+  walletInfo: {
+    flexDirection: 'row',
+    marginBottom: 10,
+  },
+  walletLabel: {
+    color: '#FFFFFF',
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginRight: 10,
+  },
+  walletValue: {
+    color: '#FFFFFF',
+    fontSize: 18,
+  },
+  balanceInfo: {
+    flexDirection: 'row',
+    marginBottom: 20,
+  },
+  balanceLabel: {
+    color: '#FFFFFF',
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginRight: 10,
+  },
+  balanceValue: {
+    color: '#FFFFFF',
+    fontSize: 18,
+  },
+  disconnectButton: {
+    backgroundColor: '#FF3B30',
+    borderRadius: 8,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+  },
+  disconnectButtonText: {
+    color: '#FFFFFF',
+    fontSize: 18,
+    fontWeight: 'bold',
   },
 });
 
