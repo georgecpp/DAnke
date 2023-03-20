2c269ed8930b4e2edc698ad3aac6e8f1a2a2ba95
George Andronache <georgeandronache.cpp@gmail.com>
2023-03-20 14:42:57 +0200

    DAnke coin redeploy

--- backend/web3/constants.js
+++ backend/web3/constants.js
@@ -1,6 +1,6 @@
-const abi = require('./Danke.json');
+const abi = require('./DAnke.json');
 const private_key = "4203abf2b9d7e6a0c99d49292c8343ab0e40a184273692051fcb9296ddc1d062";
-const contract_address = "0xf194e4C4c02C8817019c6ec8F7DC1D505317CD58";
+const contract_address = "0x49234e697BF65510B74AD3b21Ca2694C363d488d";
 
 module.exports.contractABI = abi.abi;
 module.exports.private_key = private_key;
--- client/src/web3/constants.js
+++ client/src/web3/constants.js
@@ -1,5 +1,5 @@
-const abi = require('./Danke.json');
-const contract_address = "0xf194e4C4c02C8817019c6ec8F7DC1D505317CD58";
+const abi = require('./DAnke.json');
+const contract_address = "0x49234e697BF65510B74AD3b21Ca2694C363d488d";
 
 module.exports.contractABI = abi.abi;
 module.exports.contract_address = contract_address;
